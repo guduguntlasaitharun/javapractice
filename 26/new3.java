eec475c0a4bdf7bb0e81ee4d29c7cf8bfd85b19f
guduguntlasaitharun <guduguntlasaitharun@gmail.com>
2022-12-26 19:14:53 +0530

    ok

---
+++ 26/new3.java
@@ -0,0 +1,16 @@
+class Test{
+    public static void main(String[] args){
+        int id = 102;
+        switch(id){
+            case 102:
+                System.out.println("Rahul");
+                break;
+            case 101:
+                System.out.println("Sonia");
+                break;
+
+            default:
+                System.out.println("Modi");
+        }
+    }
+}
---
+++ day01/Test6.java
@@ -0,0 +1,13 @@
+class Test{
+   public static void main(String[] args){
+     int a=5;
+	 System.out.println(a);
+   
+   
+   }
+
+
+
+
+
+  }
