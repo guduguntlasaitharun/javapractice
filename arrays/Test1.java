e9c6cbe157fef20ddd8060f73e5904eb1669cd4b
guduguntlasaitharun <guduguntlasaitharun@gmail.com>
2023-01-04 10:42:55 +0530

    ok

---
+++ arrays/Test1.java
@@ -0,0 +1,14 @@
+class Test{
+    public static void main(String args[]){
+        int ids[]=new int[6];
+        ids[0]=10;
+        ids[1]=20;
+        ids[2]=30;
+        ids[3]=40;
+        ids[4]=50;
+        ids[5]=60;
+        for(int i=0;i<=ids.length-1;i++){
+            System.out.println(ids[i]);
+        }
+    }
+}
