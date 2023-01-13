7dbd40e225710d28f1495f249d7154fd03ce0947
guduguntlasaitharun <guduguntlasaitharun@gmail.com>
2023-01-13 22:07:11 +0530

    string

---
+++ tring.java
@@ -0,0 +1,11 @@
+class Test{
+public static void main(String[] args)
+{
+ String name="Sai";
+ 
+ System.out.println(name);
+ System.out.println(name.concat("Tharun"));
+ System.out.println(name.length());
+
+ }
+}
